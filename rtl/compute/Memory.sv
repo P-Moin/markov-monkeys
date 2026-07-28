425586fd108a70012e4aebcdac144316bb535cef
P-Moin <157856347+P-Moin@users.noreply.github.com>
2026-07-28 07:22:10 -0700

    Update Memory.sv

--- rtl/compute/Memory.sv
+++ rtl/compute/Memory.sv
@@ -6,14 +6,14 @@ module Memory #(
     parameter integer DATA_D = 64,
     parameter integer ADDR_W = (DATA_D <= 1) ? 1 : $clog2(DATA_D)
 ) (
-    input  wire                       clk,
+    input  wire clk,
 
-    input  wire signed [DW-1:0]       w_data,
-    input  wire [ADDR_W-1:0]          w_address,
-    input  wire                       w_enable,
+    input  wire signed [DW-1:0] w_data,
+    input  wire [ADDR_W-1:0] w_address,
+    input  wire w_enable,
 
-    input  wire [ADDR_W-1:0]          r_address,
-    output reg  signed [DW-1:0]       r_data
+    input  wire [ADDR_W-1:0] r_address,
+    output reg  signed [DW-1:0] r_data
 );
 
     reg signed [DW-1:0] memory [0:DATA_D-1];
